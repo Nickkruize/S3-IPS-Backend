b5122b0689d4a1a3e128751f125a7eddce1e1693
Nick Kruize <n.kruize@gmail.com>
2021-11-25 20:00:07 +0100

    Rewrote seeding to use bogus.Faker and added migration apply on runtime

--- DAL/Migrations/20211125184139_CategoryNameUnique.Designer.cs
+++ DAL/Migrations/20211125184139_CategoryNameUnique.Designer.cs
@@ -3,14 +3,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(WebshopContext))]
-    partial class WebshopContextModelSnapshot : ModelSnapshot
+    [Migration("20211125184139_CategoryNameUnique")]
+    partial class CategoryNameUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
